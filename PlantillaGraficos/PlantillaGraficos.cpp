7edb26328de8e810acc4f1191cd65e500182a617
alumno <alumno@E314>
2020-01-30 07:52:02 -0700

    acabado

--- PlantillaGraficos/PlantillaGraficos.cpp
+++ PlantillaGraficos/PlantillaGraficos.cpp
@@ -33,26 +33,6 @@ void dibujarQuads() {
 	glEnd();
 }
 
-void dibujarPoligono() {
-	
-}
-
-void dibujarTrianguloContinuo() {
-	glBegin(GL_TRIANGLE_STRIP);
-
-	glColor3f(1.0f, 1.0f, 1.0f);
-
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.1f, 0.1f, 0.0f);
-	glVertex3f(0.2f, 0.0f, 0.0f);
-
-	glColor3f(0.0f, 0.7f, 1.0f);
-
-	glVertex3f(0.1f, 0.5f, 0.0f);
-
-	glEnd();
-}
-
 void dibujarLineaContinua() {
 	glBegin(GL_LINE_STRIP);
 
@@ -111,9 +91,341 @@ void dibujarLineas() {
 	glEnd();
 }
 
+void dibujarNubes() {
+	float teta;
+	//1
+	glPushMatrix();
+
+	glTranslatef(-0.22f, 0.52f, 0.0f);
+	glScalef(0.15f, 0.05f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+	//2
+	glPushMatrix();
+
+	glTranslatef(-0.07f, 0.55f, 0.0f);
+	glScalef(0.1f, 0.05f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	//3
+	glPushMatrix();
+
+	glTranslatef(0.42f, 0.8f, 0.0f);
+	glScalef(0.3f, 0.10f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	//4
+	glPushMatrix();
+
+	glTranslatef(0.6f, 0.9f, 0.0f);
+	glScalef(0.2f, 0.05f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	//5
+	glPushMatrix();
+
+	glTranslatef(0.8f, 0.5f, 0.0f);
+	glScalef(0.25f, 0.05f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	//6
+	glPushMatrix();
+
+	glTranslatef(0.9f, 0.55f, 0.0f);
+	glScalef(0.25f, 0.05f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 1.0f);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+}
+
+void dibujarSol()
+{
+	float teta;
+
+	glPushMatrix();
+
+	glTranslatef(-0.8f, 0.8f, 0.0f);
+	glScalef(0.15f, 0.15f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	//rayos
+
+	//1er par
+	glPushMatrix();
+
+	glRotatef(0.0f, 0.0f, 0.0f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	glVertex3f(-0.81f, 0.63f, 0.0f);
+	glVertex3f(-0.81f, 0.53f, 0.0f);
+	glVertex3f(-0.80f, 0.53f, 0.0f);
+	glVertex3f(-0.80f, 0.63f, 0.0f);
+
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	glVertex3f(-0.81f, 1.0f, 0.0f);
+	glVertex3f(-0.81f, 0.97f, 0.0f);
+	glVertex3f(-0.80f, 0.97f, 0.0f);
+	glVertex3f(-0.80f, 1.0f, 0.0f);
+
+	glEnd();
+
+	glPopMatrix();
+
+	//2do par
+	glPushMatrix();
+
+	glTranslatef(0.32f, 0.78f, 0.0f);
+	glRotatef(45.0f, 0.0f, 0.0f, 1.0f);
+
+
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	glVertex3f(-0.81f, 0.63f, 0.0f);
+	glVertex3f(-0.81f, 0.53f, 0.0f);
+	glVertex3f(-0.80f, 0.53f, 0.0f);
+	glVertex3f(-0.80f, 0.63f, 0.0f);
+
+	glEnd();
+
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	glVertex3f(-0.81f, 1.0f, 0.0f);
+	glVertex3f(-0.81f, 0.97f, 0.0f);
+	glVertex3f(-0.80f, 0.97f, 0.0f);
+	glVertex3f(-0.80f, 1.0f, 0.0f);
+
+	glEnd();
+
+	glPopMatrix();
+
+	//
+	glBegin(GL_POLYGON);
+	glColor3f(1.0f, 1.0f, 0.0f);
+
+	glVertex3f(-0.60f, 0.83f, 0.0f);
+	glVertex3f(-0.60f, 0.82f, 0.0f);
+	glVertex3f(-0.50f, 0.82f, 0.0f);
+	glVertex3f(-0.50f, 0.83f, 0.0f);
+
+	glEnd();
+
+}
+
+void dibujarArbol()
+{
+	glBegin(GL_TRIANGLE_STRIP);
+	glColor3f(0.37f, 0.2f, 0.10f);
+
+	glVertex3f(-0.8f, -0.1f, 0.0f);
+	glVertex3f(-0.8f, -0.7f, 0.0f);
+	glVertex3f(-0.57f, -0.7f, 0.0f);
+	glVertex3f(-0.57f, -0.1f, 0.0f);
+	glVertex3f(-0.8f, -0.1f, 0.0f);
+
+	glEnd();
+	//hojas 1
+	float teta;
+
+	glPushMatrix();
+
+	glTranslatef(-0.7f, 0.0f, 0.0f);
+	glScalef(0.25f, 0.25f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(0.20, 0.6, 0.13);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	//hojas 2
+	glPushMatrix();
+
+	glTranslatef(-0.55f, 0.15f, 0.0f);
+	glScalef(0.25f, 0.25f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(0.20, 0.6, 0.13);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+
+	//hojas 3
+	glPushMatrix();
+
+	glTranslatef(-0.6f, 0.35f, 0.0f);
+	glScalef(0.25f, 0.25f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(0.20, 0.6, 0.13);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+}
+
+void dibujarTecho() {
+	glBegin(GL_TRIANGLES);
+	glColor3f(0.2f, 0.1f, 0.07f);
+
+	glVertex3f(-0.4f, 0.1f, 0.0f);
+	glVertex3f(0.8f, 0.1f, 0.0f);
+	glVertex3f(0.2f, 0.65f, 0.0f);
+
+	glEnd();
+}
+
+void dibujarPuerta() {
+	glBegin(GL_TRIANGLE_STRIP);
+	glColor3f(0.6f, 0.6f, 0.6f);
+
+	glVertex3f(0.1f, -0.3f, 0.0f);
+	glVertex3f(0.1f, -0.7f, 0.0f);
+	glVertex3f(0.35f, -0.7f, 0.0f);
+	glVertex3f(0.35f, -0.3f, 0.0f);
+	glVertex3f(0.1f, -0.3f, 0.0f);
+
+	glEnd();
+
+	float teta;
+
+	glPushMatrix();
+
+	glTranslatef(0.3f, -0.45f, 0.0f);
+	glScalef(0.025f, 0.025f, 1.0f);
+
+	glBegin(GL_POLYGON);
+	glColor3f(0.3f, 0.3f, 0.3f);
+
+	for (int i = 0; i < 360; i++)
+	{
+		teta = i * 3.1416 / 180;
+		glVertex3f(cos(teta), sin(teta), 0.0f);
+	}
+
+	glEnd();
+
+	glPopMatrix();
+}
+
+
 void dibujar() {
 	dibujarQuads();
+	dibujarTecho();
+	dibujarPuerta();
 	dibujarTriangulos();
+	dibujarSol();
+	dibujarNubes();
+	dibujarArbol();
 }
 
 int main()
